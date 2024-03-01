fe5e0beb92dd0f59b621ee7fa0c56950d64c3aac
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-03-01 16:57:31 -0500

    completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Home from "./pages/Home";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import routes from './routes';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Home />);
+const router = createBrowserRouter(routes);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<RouterProvider router={router} />);
